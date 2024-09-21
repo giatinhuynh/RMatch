a45ceb851edd883bb952223c7cd37ede141ca691
nhocbalu123 <85002041+nhocbalu123@users.noreply.github.com>
2024-09-21 21:23:50 +1000

    make UI for Swipe History
    bonus Violet Evergarden

--- rmatch/next.config.mjs
+++ rmatch/next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+/** @type {import('next').NextConfig} */  
+const nextConfig = {  
+    images: {  
+      domains: ['pwnxutdwtdqsdtkbhtfa.supabase.co', 'i0.wp.com'],  
+    },  
+  };  
+  
+  export default nextConfig;
